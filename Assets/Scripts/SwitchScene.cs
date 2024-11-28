7424e2574b91488d77ced23a8d9e66bc35d5170c
Victoria Bamurange <v.bamurange@alustudent.com>
2024-11-28 11:47:18 +0200

    the 4

--- Assets/Scripts/SwitchScene.cs
+++ Assets/Scripts/SwitchScene.cs
@@ -4,8 +4,6 @@ using UnityEngine.UI;
 
 public class SwitchScene : MonoBehaviour
 {
-    public Slider volumeSlider;
-    private AudioSource[] audioSources;
 
     void Start()
     {
@@ -20,11 +18,11 @@ public class SwitchScene : MonoBehaviour
 
     public void QuitGameMethod()
     {
-        // If we're in the editor, stop the play mode
+        
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
-        // If we're not in the editor, quit the game
+        
         Application.Quit();
 #endif
     }
